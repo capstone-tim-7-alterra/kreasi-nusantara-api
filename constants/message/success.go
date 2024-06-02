350cba0547c7bdab8f17779fb138f1b0ecaafc0e
Satrya Nararya <94729191+satryanararya@users.noreply.github.com>
2024-06-02 23:52:31 +0700

    feat: user addresses (create, update, delete, retrieve)

--- constants/message/success.go
+++ constants/message/success.go
@@ -10,4 +10,8 @@ const (
 	GET_PROFILE_SUCCESS = "user profile retrieved successfully!"
 	UPDATE_PROFILE_SUCCESS = "user profile updated successfully!"
 	DELETE_PROFILE_SUCCESS = "user profile deleted successfully!"
+	GET_USER_ADRESSES_SUCCESS = "user addresses retrieved successfully!"
+	CREATE_USER_ADDRESSES_SUCCESS = "user addresses created successfully!"
+	UPDATE_USER_ADDRESSES_SUCCESS = "user addresses updated successfully!"
+	DELETE_USER_ADDRESSES_SUCCESS = "user addresses deleted successfully!"
 )
