9d7b277b30fe1dde75a77e7e7e84f9f3d5d015e4
Satrya Nararya <94729191+satryanararya@users.noreply.github.com>
2024-06-02 16:13:58 +0700

    fix: role middleware setup

--- routes/admin/admin.go
+++ routes/admin/admin.go
@@ -2,17 +2,20 @@ package admin
 
 import (
 	// "kreasi-nusantara-api/controllers"
-	"kreasi-nusantara-api/middlewares"
+
 	// "kreasi-nusantara-api/repositories"
 	// "kreasi-nusantara-api/usecases"
 	// "kreasi-nusantara-api/utils/password"
+	"kreasi-nusantara-api/middlewares"
+	"kreasi-nusantara-api/utils/token"
 	"kreasi-nusantara-api/utils/validation"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
-func InitUserRoute(g *echo.Group, db *gorm.DB, v *validation.Validator) {
+func InitAdminRoute(g *echo.Group, db *gorm.DB, v *validation.Validator) {
 	// userRepo := repositories.NewUserRepository(db)
 	// passwordUtil := password.NewPasswordUtil()
 
@@ -20,6 +23,8 @@ func InitUserRoute(g *echo.Group, db *gorm.DB, v *validation.Validator) {
 	// userController := controllers.NewUserController(userUseCase, v)
 
 	// Protected routes
-	g.Use(middlewares.Auth, middlewares.IsAdmin)
-	// g.POST("/register", adminController.Register)
+	g.Use(echojwt.WithConfig(token.GetJWTConfig()), middlewares.IsAdmin)
+	g.GET("/tes", func(c echo.Context) error {
+		return c.JSON(200, "tes")
+	})
 }
