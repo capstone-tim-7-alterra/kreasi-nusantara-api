94075d4da60d90a79debd117d7c844859065d652
Satrya Nararya <94729191+satryanararya@users.noreply.github.com>
2024-06-02 16:13:36 +0700

    feat: user authentication feature
    - Login
    - Forgot Password
    - Reset Password
    - Get Profile
    - Update Profile

--- dto/user/user.go
+++ dto/user/user.go
@@ -1,11 +1,13 @@
 package dto
 
+import "time"
+
 type RegisterRequest struct {
 	FirstName string `json:"first_name" validate:"required"`
 	LastName  string `json:"last_name" validate:"required"`
 	Username  string `json:"username" validate:"required"`
 	Email     string `json:"email" validate:"required,email"`
-	Password  string `json:"password" validate:"required,min=8,max=32,alphanum"`
+	Password  string `json:"password" validate:"required,min=8,max=32"`
 }
 
 type RegisterResponse struct {
@@ -16,4 +18,44 @@ type RegisterResponse struct {
 type VerifyOTPRequest struct {
 	Email string `json:"email"`
 	OTP   string `json:"otp"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8,max=32"`
+}
+
+type LoginResponse struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+}
+
+type ForgotPasswordRequest struct {
+	Email string `json:"email" validate:"required,email"`
+}
+
+type ResetPasswordRequest struct {
+	Email              string `json:"email" validate:"required,email"`
+	NewPassword        string `json:"new_password" validate:"required,min=8,max=32"`
+	ConfirmNewPassword string `json:"confirm_new_password" validate:"required,min=8,max=32"`
+}
+
+type UserProfileResponse struct {
+	Username    string     `json:"username"`
+	FirstName   string     `json:"first_name"`
+	LastName    string     `json:"last_name"`
+	Email       string     `json:"email"`
+	Phone       *string    `json:"phone"`
+	Photo       *string    `json:"photo"`
+	Gender      *string    `json:"gender"`
+	DateOfBirth *time.Time `json:"date_of_birth"`
+}
+
+type UpdateProfileRequest struct {
+	FirstName   *string     `json:"first_name" validate:"required"`
+	LastName    *string     `json:"last_name" validate:"required"`
+	Phone       *string    `json:"phone"`
+	Gender      *string    `json:"gender"`
+	DateOfBirth *time.Time `json:"date_of_birth"`
 }
--- routes/user/user_route.go
+++ routes/user/user_route.go
@@ -3,13 +3,17 @@ package user
 import (
 	"kreasi-nusantara-api/controllers"
 	"kreasi-nusantara-api/drivers/redis"
+
 	// "kreasi-nusantara-api/middlewares"
 	"kreasi-nusantara-api/repositories"
 	"kreasi-nusantara-api/usecases"
 	"kreasi-nusantara-api/utils/email"
 	"kreasi-nusantara-api/utils/otp"
 	"kreasi-nusantara-api/utils/password"
+	"kreasi-nusantara-api/utils/token"
 	"kreasi-nusantara-api/utils/validation"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -21,15 +25,20 @@ func InitUserRoute(g *echo.Group, db *gorm.DB, v *validation.Validator) {
 	passwordUtil := password.NewPasswordUtil()
 	otpUtil := otp.NewOTPUtil()
 	emailUtil := email.NewEmailUtil()
+	tokenUtil := token.NewTokenUtil()
 
-	userUseCase := usecases.NewUserUseCase(userRepo, passwordUtil, *redisClient, otpUtil, emailUtil)
-	userController := controllers.NewUserController(userUseCase, v)
+	userUseCase := usecases.NewUserUseCase(userRepo, passwordUtil, *redisClient, otpUtil, emailUtil, tokenUtil)
+	userController := controllers.NewUserController(userUseCase, v, tokenUtil)
 
 	// Public routes
 	g.POST("/register", userController.Register)
 	g.POST("/verify-otp", userController.VerifyOTP)
+	g.POST("/login", userController.Login)
+	g.POST("/forgot-password", userController.ForgotPassword)
+	g.POST("/reset-password", userController.ResetPassword)
 
 	// Protected routes
-	// g.Use(middlewares.Auth)
-	// g.GET("/profile", userController.GetProfile)
+	g.Use(echojwt.WithConfig(token.GetJWTConfig()))
+	g.GET("/users/me", userController.GetProfile)
+	g.PUT("/users/me", userController.UpdateProfile)
 }
